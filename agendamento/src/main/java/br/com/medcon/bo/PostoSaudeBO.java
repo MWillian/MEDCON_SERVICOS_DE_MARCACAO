b176b7253a21c82aba4938383f4643c65fd40ba2
MWillian <matheuswno02@gmail.com>
2025-12-08 19:36:41 -0300

    Craido PostoSaudeBO, criado seus métodos e verificações. Ajustada View.

--- agendamento/src/main/java/br/com/medcon/bo/PostoSaudeBO.java
+++ agendamento/src/main/java/br/com/medcon/bo/PostoSaudeBO.java
@@ -3,29 +3,41 @@ package br.com.medcon.bo;
 import java.sql.SQLException;
 import java.util.List;
 
+import br.com.medcon.bo.exception.NegocioException;
 import br.com.medcon.dao.PostoSaudeDAO;
 import br.com.medcon.vo.PostoSaude;
 
 public class PostoSaudeBO {
-    private final PostoSaudeDAO postoSaudeDAO;
 
-    public PostoSaudeBO(PostoSaudeDAO postoSaudeDAO) {
-        this.postoSaudeDAO = postoSaudeDAO;
+    private final PostoSaudeDAO dao;
+
+    public PostoSaudeBO(PostoSaudeDAO dao) {
+        this.dao = dao;
     }
 
-    public void salvar(PostoSaude ps) throws SQLException {
-        postoSaudeDAO.salvar(ps);
-    };
-    public void atualizar(PostoSaude ps) throws SQLException {
-        postoSaudeDAO.atualizar(ps);
-    };
-    public void deletar(int id) throws SQLException {
-        postoSaudeDAO.deletar(id);
-    };
-    public PostoSaude buscarPorId(int id) throws SQLException{ 
-        return postoSaudeDAO.buscarPorId(id);
-    };
-    public List<PostoSaude> listarTodos() throws SQLException { 
-        return postoSaudeDAO.listarTodos();
-    };
+    public void salvar(PostoSaude posto) throws NegocioException, SQLException {
+        ValidarCamposObrigatorios(posto);
+        dao.salvar(posto);
+    }
+
+    public List<PostoSaude> listarTodos() throws SQLException {
+        return dao.listarTodos();
+    }
+
+    public PostoSaude buscarPorId(int id) throws SQLException, NegocioException {
+        PostoSaude posto = dao.buscarPorId(id); 
+        if (posto == null) {
+            throw new NegocioException("Posto não encontrado.");
+        }
+        return posto;
+    }
+
+    public void ValidarCamposObrigatorios(PostoSaude posto) throws NegocioException{
+        if (posto.getNome() == null || posto.getNome().trim().length() < 10) {
+            throw new NegocioException("Nome do Posto é obrigatório (min 10 caracteres).");
+        }
+        if (posto.getEndereco() == null || posto.getEndereco().trim().length() < 5) {
+            throw new NegocioException("Endereço é obrigatório.");
+        }
+    }
 }
